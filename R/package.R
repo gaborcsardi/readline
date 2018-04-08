48cd40f6eaa202f9f426c924b04517bceda579d6
Gábor Csárdi <csardi.gabor@gmail.com>
2018-04-08 17:28:28 +0100

    Default values and hints

--- R/package.R
+++ R/package.R
@@ -5,14 +5,16 @@
 #'   line of the user input. Ideally it should be short, so there
 #'   is enough space for the input. It can be colored and styled
 #'   with ANSI escape sequences from the \code{crayon} package.
+#' @param default Default value.
 #' @param history If not \code{NULL}, then it must be a character
 #'   scalar, the name of the file that is used as a browseable history. The
 #'   newly entered entry is added to this file. Note that history
 #'   might not be supported by the \code{base::readline} fallback
 #'   method.
-#' @param completions A character vector of possible TAB-completions.
-#'   Ignored on unsupported terminals that use the \code{base::readline}
-#'   fallback mechanism.
+#' @param completions A character vector of possible TAB-completions or
+#'   hints.
+#' @param show_hints Whether to show hints.
+#' @param tab_complete Whether to activate TAB completion.
 #' @return A character scalar, the string that was read.
 #'
 #' @export
@@ -34,14 +36,19 @@
 #'   read_line(prompt = "what> ", completions = c("foobar", "foo", "bar"))
 #' }
 
-read_line <- function(prompt = "? ", history = NULL,
-                      completions = character()) {
+read_line <- function(prompt = "? ", default = "", history = NULL,
+                      completions = character(), show_hints = TRUE,
+                      tab_complete = FALSE) {
 
+  default <- as_string(default)
   if (!is.null(history)) history <- as_string(history)
   completions <- as.character(completions)
+  show_hints <- as_flag(show_hints)
+  tab_complete <- as_flag(tab_complete)
 
   res <- tryCatch(
-    .Call(C_read_line, prompt, history, completions),
+    .Call(C_read_line, prompt, default, history, completions, show_hints,
+          tab_complete),
     interrupt = function(e) stop("Interrupted")
   )
   if (is.null(res)) stop("Interrupted")
--- src/api.c
+++ src/api.c
@@ -17,11 +17,24 @@ void completion_hook(char const* prefix, int bp,
   }
 }
 
-SEXP read_line(SEXP prompt, SEXP history, SEXP completions) {
+void hint_hook(char const* prefix, int bp, replxx_hints* lc,
+	       ReplxxColor* c, void* ud) {
+  SEXP str = (SEXP) ud;
+  size_t i, n = LENGTH(str);
+  size_t len = strlen(prefix);
+  for (i = 0; i < n; i++) {
+    const char *s = CHAR(STRING_ELT(str, i));
+    if (! strncmp(prefix, s, len)) replxx_add_hint(lc, s + len);
+  }
+}
+
+SEXP read_line(SEXP prompt, SEXP default_, SEXP history, SEXP completions,
+	       SEXP show_hints, SEXP tab_complete) {
 
   SEXP result = R_NilValue;
   Replxx* replxx = replxx_init();
   const char *c_history = isNull(history) ? 0 : CHAR(STRING_ELT(history, 0));
+  const char *c_default = CHAR(STRING_ELT(default_, 0));
   replxx_install_window_change_handler(replxx);
 
   if (c_history) {
@@ -31,7 +44,14 @@ SEXP read_line(SEXP prompt, SEXP history, SEXP completions) {
     }
   }
 
-  replxx_set_completion_callback(replxx, completion_hook, completions);
+  replxx_set_preload_buffer(replxx, c_default);
+
+  if (LOGICAL(show_hints)[0]) {
+    replxx_set_hint_callback(replxx, hint_hook, completions);
+  }
+  if (LOGICAL(tab_complete)[0]) {
+    replxx_set_completion_callback(replxx, completion_hook, completions);
+  }
 
   const char* c_result = replxx_input(replxx, CHAR(STRING_ELT(prompt, 0)));
 
@@ -48,7 +68,7 @@ SEXP read_line(SEXP prompt, SEXP history, SEXP completions) {
 
 
 static const R_CallMethodDef callMethods[]  = {
-  {"read_line", (DL_FUNC) &read_line, 3},
+  {"read_line", (DL_FUNC) &read_line, 6},
   {NULL, NULL, 0}
 };
 
